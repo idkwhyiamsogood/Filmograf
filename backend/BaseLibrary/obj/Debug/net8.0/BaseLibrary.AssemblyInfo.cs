d6dca28c665acda1433668ff59b04708c77bf6e5
SANE5K <84617085+aleksandrresetnikov@users.noreply.github.com>
2026-03-27 15:23:02 +0300

    фиксы

--- backend/BaseLibrary/obj/Debug/net8.0/BaseLibrary.AssemblyInfo.cs
+++ backend/BaseLibrary/obj/Debug/net8.0/BaseLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Filmograf.BaseLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e384a1672ccc6fa60fc6a6063b9601fedca48273")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d14f5dbc3dd736c7527adcfc89d709c4007d02d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Filmograf.BaseLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Filmograf.BaseLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- test-front/lib/axios.ts
+++ test-front/lib/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const authLink =
-  // process.env.NEXT_PUBLIC_API_URL || "https://filmograf.online";
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:5090";
+  process.env.NEXT_PUBLIC_API_URL || "https://filmograf.online";
+  // process.env.NEXT_PUBLIC_API_URL || "http://localhost:5090";
 
 const settingUpAxiosInstance = (link: string) => {
   const axiosInstance = axios.create({
