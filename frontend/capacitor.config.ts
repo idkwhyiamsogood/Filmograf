32c075198db2e41713669d948aff3c42a32f0ccf
SANE5K <84617085+aleksandrresetnikov@users.noreply.github.com>
2026-04-01 19:59:35 +0300

    asdfghjkkl;'

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -3,9 +3,9 @@ import type { CapacitorConfig } from "@capacitor/cli";
 const config: CapacitorConfig = {
   appId: "com.filmograf",
   appName: "Filmograf",
-
+webDir: 'out',
   server: {
-    url: "http://127.0.0.1:3000",
+    url: "http://26.254.134.172:3000",
     androidScheme: "http",
     cleartext: true,
   },
